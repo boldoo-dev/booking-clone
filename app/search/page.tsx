3c6a853e03f0fecc1c4472f4216c9a4047ac9cac
Bold-Erdene Jigmed <77866161+boldoo-dev@users.noreply.github.com>
2025-02-09 20:30:38 +0800

    Scraping Data from Booking.com using Oxylabs

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,9 +1,10 @@
+import { fetchResults } from "@/lib/fetchResults";
 import { notFound } from "next/navigation";
 
 type Props = {
   searchParams: SearchParams;
 };
-type SearchParams = {
+export type SearchParams = {
   url: URL;
   group_adults: string;
   group_children: string;
@@ -11,9 +12,12 @@ type SearchParams = {
   checkin: string;
   checkout: string;
 };
-function SearchPage({ searchParams }: Props) {
+async function SearchPage({ searchParams }: Props) {
   if (!searchParams.url) return notFound();
-  
+
+  const results = await fetchResults(searchParams);
+  // if (!results) return <div>No results...</div>;
+
   return <div>SearchPage</div>;
 }
 
